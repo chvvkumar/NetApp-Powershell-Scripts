f0f5fb3e31d8fc5c9a7ffa0d4e96016320514103
Kumar Challa <chvvkumar@users.noreply.github.com>
2021-03-05 09:08:29 -0600

    Update clone.ps1

--- clone.ps1
+++ clone.ps1
@@ -89,3 +89,7 @@ Write-Host "Mounted new clone to junction-path: $((get-ncvol $clone_volume).Junc
 # modify export policy
 Update-NcVol -query @{name="$clone_old"} -Attributes @{volumeexportattributes=@{policy="$clone_export_policy"}} | Out-Null
 Write-Host "Modified export policy of: $($clone_old) to: $((get-ncvol $clone_volume).VolumeExportAttributes.Policy)"
+
+# Split the clone off
+Start-NcVolCloneSplit -VserverContext $vserver -Name $clone_old | Out-Null
+Write-Host "Splitting: $($clone_old) from: $($parent_volume)"
